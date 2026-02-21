64207951dfe21aa8f7e03326d23d54157fbb1a34
kaprmna <ddikaprmna@gmail.com>
2026-02-20 21:12:14 -0800

    replace

--- index.js
+++ index.js
@@ -1,70 +1,76 @@
 const express = require('express');
+const app = express();
+const rateLimit = require('express-rate-limit');
 const bodyParser = require('body-parser');
-const rateLimiter = require('express-rate-limit');
 const compression = require('compression');
-const path = require('path');
 
-const server = express();
+const ipNow = "134.199.147.136";
+const hostdo = 
+`134.199.147.136 growtopia1.com
+134.199.147.136 growtopia2.com
+134.199.147.136 www.growtopia1.com
+134.199.147.136 www.growtopia2.com
+134.199.147.136 growtopiagame.com
+`;
 
-//SET SECURITY
-server.use(compression({
+app.use(compression({
     level: 5,
-    threshold: 0,
     filter: (req, res) => {
-        if (req.headers['x-no-compression']) {
-            return false;
-        }
+        if (req.headers['X-No-Compression']) return false;
         return compression.filter(req, res);
     }
 }));
-server.set('trust proxy', 1);
-server.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept',
-    );
-    console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url} - ${res.statusCode}`);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    console.log(`${req.method} request for '${req.url}' - ${JSON.stringify(req.body)} | Status: ${res.statusCode}`);
     next();
 });
-server.use(express.static("public"));
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use(express.json());
-server.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
+app.use(express.json());
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: 'Too many requests from this IP, please try again after an hour',
+}));
+app.set('trust proxy', 1);
+app.use(express.static(__dirname + '/public'));
 
-//SET API
-
-
-
-//SET ROUTE
-server.get('/', function (req, res) {
-     let tempUrl = 'fancytest';
-    const hostname = req.hostname;
-
-    switch (hostname) {
-
-        case 'fancytest.vercel.app':
-            return res.sendFile(
-                path.join(__dirname + '/html/main/index.html')
-            );
-
-        case 'elvaraclass.fancycdn.fun':
-        case `elvaraclass.${tempUrl}.vercel.app`:
-            return res.sendFile(
-                path.join(__dirname, '../html/elvara/dist/index.html')
-            );
-
-        case 'ddika.fancycdn.fun':
-        case `ddika.${tempUrl}.vercel.app`:
-            return res.send('Portfolio are maintenance');
-
-        default:
-            return res.status(404).send('Unknown domain');
+app.use((req, res, next) => {
+    if (req.url.match(/\.(js|css)$/i)) {
+        const referer = req.get('referer');
+        if (referer && !referer.startsWith(req.protocol + '://' + req.get('host'))) {
+            return res.status(403).send('Forbidden');
+        }
     }
+    next();
 });
 
-//START SERVER
-const runPort = 5000;
-server.listen(runPort, function () {
-    console.log(`Server Running On Port: ${runPort}`);
+app.all('/ios', (req, res) => {
+     res.set('Content-Type', 'text/plain');
+    res.send(
+        `[General]
+
+[Rule]
+FINAL,DIRECT
+
+[Host]
+www.growtopia2.com = ${ipNow}
+www.growtopia1.com = ${ipNow}
+growtopia2.com = ${ipNow}
+growtopia1.com = ${ipNow}
+        `
+    );
+});
+app.all('/android', (req, res) => {
+    res.set('Content-Type', 'text/plain');
+    res.send(hostdo);
+});
+
+app.all('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(5000, function () {
+    console.log(`Listening on port 5000`);
 });
